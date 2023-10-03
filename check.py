94979b3cefe084d23792c41059851f9139df8be3
jfpiedad <jfpiedad@gmail.com>
2023-10-03 22:53:05 +0800

    refactored some code and changed 'url' to 'manga_url' for clarity'

--- check.py
+++ check.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from tinydb import TinyDB, Query
+from tinydb import TinyDB
 from urllib.parse import urlparse
 
 import asyncio
@@ -12,14 +12,17 @@ async def check_new_chapters():
     """
     Checks if there are new manga chapters available.
     """
-    url = 1
-    mangas = [entry for entry in get_manga_titles()]
+    last_read_chapter = 1
+    url = 2
+    mangas_info = [info for info in get_manga_info()]
 
     tasks = []
 
-    for manga in mangas:
+    for manga in mangas_info:
         html_data = await fetch_manga_data(manga[url])
-        task = asyncio.create_task(parse_data(html_data, manga[url]))
+        task = asyncio.create_task(
+            parse_data(html_data, manga[last_read_chapter], manga[url])
+        )
         tasks.append(task)
 
     # html_data = await fetch_manga_data(
@@ -28,6 +31,7 @@ async def check_new_chapters():
     # task = asyncio.create_task(
     #     parse_data(
     #         html_data,
+    #         "14",
     #         "https://immortalupdates.com/manga/i-became-an-evolving-space-monster/",
     #     )
     # )
@@ -35,7 +39,7 @@ async def check_new_chapters():
 
     results = await asyncio.gather(*tasks)
 
-    for manga, total in zip(mangas, results):
+    for manga, total in zip(mangas_info, results):
         if total != 0:
             print(f"{manga[0]} : {total} new chapters")
 
@@ -43,36 +47,34 @@ async def check_new_chapters():
         print("There Are No New Chapters At The Moment.")
 
 
-async def fetch_manga_data(url):
+async def fetch_manga_data(manga_url):
     """
     Fetches the HTML of the website.
     """
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
+        async with session.get(manga_url) as response:
             return await response.text()
 
 
-async def parse_data(html_data, url):
+async def parse_data(html_data, last_read_chapter, manga_url):
     """
     Parse the HTML data using Beautiful Soup.
     """
-    classification = classify_manga_url(url)
+    classification = classify_manga_url(manga_url)
 
     soup = BeautifulSoup(html_data, "html.parser")
 
     if classification in ["manganato", "chapmanganato"]:
         return parse_similar_data(
-            soup, div_class="story-info-right", ul_class="row-content-chapter"
+            soup, last_read_chapter=last_read_chapter, ul_class="row-content-chapter"
         )
     elif classification == "immortalupdates":
-        return parse_similar_data(soup, div_class="post-title", ul_class="version-chap")
+        return parse_similar_data(
+            soup, last_read_chapter=last_read_chapter, ul_class="version-chap"
+        )
     elif classification == "culturedworks":
-        manga_title = soup.find("div", class_="info-desc").find("h1")
-
         chapters_section = soup.find("div", class_="eplister")
         list_of_chapters = chapters_section.find_all("span", class_="chapternum")
-
-        last_read_chapter = get_last_read_chapter(manga_title.text)
 
         count = 0
         for chapter in list_of_chapters:
@@ -100,12 +102,8 @@ def parse_similar_data(soup: BeautifulSoup, **kwargs):
     Parse HTML Data with similar formats.\n
     Created this function to reduce boilerplate code.
     """
-    manga_title = soup.find("div", class_=kwargs["div_class"]).find("h1")
-
     chapters_section = soup.find("ul", class_=kwargs["ul_class"])
     list_of_chapters = chapters_section.find_all("a")
-
-    last_read_chapter = get_last_read_chapter(manga_title.text)
 
     count = 0
     for chapter in list_of_chapters:
@@ -116,9 +114,9 @@ def parse_similar_data(soup: BeautifulSoup, **kwargs):
                 .group(0)
                 .split(" ")[1]
             )
-            if chapter_number != last_read_chapter:
+            if chapter_number != kwargs["last_read_chapter"]:
                 count += 1
-            elif chapter_number == last_read_chapter:
+            elif chapter_number == kwargs["last_read_chapter"]:
                 break
         except IndexError:
             continue
@@ -128,34 +126,21 @@ def parse_similar_data(soup: BeautifulSoup, **kwargs):
     return count
 
 
-def classify_manga_url(url):
+def classify_manga_url(manga_url):
     """
     Classifies different manga websites for parsing data.\n
     Websites have different format, so it is classified to
     parse according to the classification.
     """
-    parsed_url = urlparse(url)
-    domain = parsed_url.netloc
+    parsed_manga_url = urlparse(manga_url)
+    domain = parsed_manga_url.netloc
 
-    parsed_url_name = domain.split(".")[0]
+    parsed_manga_url_name = domain.split(".")[0]
 
-    return parsed_url_name.strip()
-
-
-def get_last_read_chapter(title):
-    """
-    Retrieves last read chapter of a given title.
-    """
-    Manga = Query()
-    with TinyDB("mangas.json") as db:
-        try:
-            manga = db.get(Manga.title == title.strip())
-            return manga["last_read_chapter"].strip()
-        except Exception as e:
-            return f"An error occurred when retrieving the last read chapter. {e}"
+    return parsed_manga_url_name.strip()
 
 
-def get_manga_titles():
+def get_manga_info():
     """
     Retrieves all manga info.\n
     Title and url are extracted.
@@ -164,7 +149,9 @@ def get_manga_titles():
         titles_arr = []
         try:
             for entry in db.all():
-                titles_arr.append([entry["title"], entry["url"]])
+                titles_arr.append(
+                    [entry["title"], entry["last_read_chapter"], entry["url"]]
+                )
         except Exception as e:
             return f"An error occurred when retrieving the manga title. {e}"
 
