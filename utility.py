fe45cf4d9acb664f9aea5b4e66f2c7f9bcf079c6
jfpiedad <jfpiedad@gmail.com>
2023-10-04 22:54:14 +0800

    added new functions

--- utility.py
+++ utility.py
@@ -1,5 +1,7 @@
 import re
+
 import validators
+from tinydb import TinyDB, Query
 
 
 def check_manga_title_validity(manga_title):
@@ -41,13 +43,41 @@ def check_manga_url_validity(manga_url):
     return False
 
 
-def add_manga_to_list():
-    pass
+def add_manga_in_list(manga_info):
+    with TinyDB("mangas.json", indent=4, separators=(",", ": ")) as db:
+        db.insert(manga_info)
+
+
+def update_manga_in_list(manga_info):
+    manga_regex = manga_title_regex_matcher(manga_info["title"])
+    Manga = Query()
+    with TinyDB("mangas.json", indent=4, separators=(",", ": ")) as db:
+        db.update(
+            {"last_read_chapter": manga_info["last_chapter"]},
+            Manga.title.matches(manga_regex, re.IGNORECASE),
+        )
+
+
+def check_manga_in_list(manga_title):
+    manga_regex = manga_title_regex_matcher(manga_title)
+    Manga = Query()
+
+    with TinyDB("mangas.json") as db:
+        is_present = db.contains(Manga.title.matches(manga_regex, re.IGNORECASE))
+
+    return is_present
+
+
+def manga_title_regex_matcher(manga_title):
+    regex = r""
+
+    for manga in manga_title.split():
+        regex += rf"{re.escape(manga)}\s"
+
+    regex = regex.rstrip("\\s")
+
+    return regex
 
 
 if __name__ == "__main__":
-    print(
-        check_manga_url_validity(
-            "https://github.com/jfpiedad/manga-checker#manga-checker"
-        )
-    )
+    print(check_manga_in_list("I became an evolving space monster"))
