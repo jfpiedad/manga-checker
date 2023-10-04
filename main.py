60e5685b6957b88c27e3832012af12b77d4359c9
jfpiedad <jfpiedad@gmail.com>
2023-10-04 22:57:54 +0800

    update command working, needs refactoring, try to change to upsert, and make some update command inputs to optional

--- main.py
+++ main.py
@@ -18,10 +18,7 @@ args = parser.parse_args()
 if __name__ == "__main__":
     if args.command == "check":
         start = time.time()
-        try:
-            asyncio.run(check_new_chapters())
-        except Exception as e:
-            print(f"There was an error: {e}")
+        asyncio.run(check_new_chapters())
         end = time.time() - start
         print(f"Time To Finish: {end: .2f}s")
     elif args.command == "add":
@@ -55,9 +52,47 @@ if __name__ == "__main__":
                 "url": manga_url,
             }
 
-            utility.add_manga_to_list(manga_info)
-            print("Manga Successfully added to the list!")
+            utility.add_manga_in_list(manga_info)
+            print("Manga successfully added to the list!")
         except KeyboardInterrupt:
-            print("\nAdding manga is cancelled. Ok bye!")
+            print("\nAdding manga is cancelled.")
     elif args.command == "update":
-        pass
+        try:
+            while True:
+                manga_title = input("Manga Title: ")
+
+                if not utility.check_manga_title_validity(manga_title):
+                    print("Title cannot be an empty string!")
+                    continue
+
+                if utility.check_manga_in_list(manga_title):
+                    break
+
+                print("Manga is not in the list!")
+
+            while True:
+                last_read_chapter = input("Last Chapter Read: ")
+
+                if utility.check_chapter_number_validity(last_read_chapter):
+                    break
+
+                print("Please enter a valid number between 0 and 1000!")
+
+            while True:
+                manga_url = input("Manga URL: ")
+
+                if utility.check_manga_url_validity(manga_url):
+                    break
+
+                print("Please enter a valid URL!")
+
+            manga_info = {
+                "title": manga_title,
+                "last_chapter": last_read_chapter,
+                "url": manga_url,
+            }
+
+            utility.update_manga_in_list(manga_info)
+            print("Manga successfully updated to the list!")
+        except KeyboardInterrupt:
+            print("\nUpdating manga is cancelled.")
