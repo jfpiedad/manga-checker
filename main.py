8f4d6db5a49fdd3cdf53740375a5b6585bf66175
jfpiedad <jfpiedad@gmail.com>
2023-10-06 01:07:44 +0800

    okay na ang uban commands, delete nalay kuwang

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import argparse
 import asyncio
 import time
 
-import utility
 from check import check_new_chapters
+from commands import add_manga, update_manga
 
 
 parser = argparse.ArgumentParser(description="Check New Manga Chapters")
@@ -11,7 +11,7 @@ subparser = parser.add_subparsers(title="commands", dest="command")
 
 check_parser = subparser.add_parser("check", help="check for new chapters")
 add_parser = subparser.add_parser("add", help="Add new manga to the list")
-update_parser = subparser.add_parser("update", help="update last read chapters")
+update_parser = subparser.add_parser("update", help="update last read chapters or url")
 
 args = parser.parse_args()
 
@@ -22,77 +22,6 @@ if __name__ == "__main__":
         end = time.time() - start
         print(f"Time To Finish: {end: .2f}s")
     elif args.command == "add":
-        try:
-            while True:
-                manga_title = input("Manga Title: ")
-
-                if utility.check_manga_title_validity(manga_title):
-                    break
-
-                print("Title cannot be an empty string!")
-            while True:
-                last_read_chapter = input("Last Chapter Read: ")
-
-                if utility.check_chapter_number_validity(last_read_chapter):
-                    break
-
-                print("Please enter a valid number between 0 and 1000!")
-
-            while True:
-                manga_url = input("Manga Website URL: ")
-
-                if utility.check_manga_url_validity(manga_url):
-                    break
-
-                print("Please enter a valid URL!")
-
-            manga_info = {
-                "title": manga_title,
-                "last_chapter": last_read_chapter,
-                "url": manga_url,
-            }
-
-            utility.add_manga_in_list(manga_info)
-            print("Manga successfully added to the list!")
-        except KeyboardInterrupt:
-            print("\nAdding manga is cancelled.")
+        add_manga()
     elif args.command == "update":
-        try:
-            while True:
-                manga_title = input("Manga Title: ")
-
-                if not utility.check_manga_title_validity(manga_title):
-                    print("Title cannot be an empty string!")
-                    continue
-
-                if utility.check_manga_in_list(manga_title):
-                    break
-
-                print("Manga is not in the list!")
-
-            while True:
-                last_read_chapter = input("Last Chapter Read: ")
-
-                if utility.check_chapter_number_validity(last_read_chapter):
-                    break
-
-                print("Please enter a valid number between 0 and 1000!")
-
-            while True:
-                manga_url = input("Manga URL: ")
-
-                if utility.check_manga_url_validity(manga_url):
-                    break
-
-                print("Please enter a valid URL!")
-
-            manga_info = {
-                "title": manga_title,
-                "last_chapter": last_read_chapter,
-                "url": manga_url,
-            }
-
-            utility.update_manga_in_list(manga_info)
-            print("Manga successfully updated to the list!")
-        except KeyboardInterrupt:
-            print("\nUpdating manga is cancelled.")
+        update_manga()
