4b65be45352262cf2f327a2957e548c3ce9953ee
jfpiedad <jfpiedad@gmail.com>
2023-10-06 01:06:00 +0800

    gibalhin ang uban functions sa `commands.py` para limpyo

--- utility.py
+++ utility.py
@@ -4,14 +4,14 @@ import validators
 from tinydb import TinyDB, Query
 
 
-def check_manga_title_validity(manga_title):
+def manga_title_validity(manga_title):
     if manga_title:
         return True
 
     return False
 
 
-def check_chapter_number_validity(chapter_number):
+def manga_chapter_validity(chapter_number):
     integer_pattern = r"^\d+$"
     special_pattern = r"\d+([.-]\d+)+"
 
@@ -34,7 +34,7 @@ def check_chapter_number_validity(chapter_number):
     return False
 
 
-def check_manga_url_validity(manga_url):
+def manga_url_validity(manga_url):
     is_valid = validators.url(manga_url)
 
     if is_valid:
@@ -43,22 +43,7 @@ def check_manga_url_validity(manga_url):
     return False
 
 
-def add_manga_in_list(manga_info):
-    with TinyDB("mangas.json", indent=4, separators=(",", ": ")) as db:
-        db.insert(manga_info)
-
-
-def update_manga_in_list(manga_info):
-    manga_regex = manga_title_regex_matcher(manga_info["title"])
-    Manga = Query()
-    with TinyDB("mangas.json", indent=4, separators=(",", ": ")) as db:
-        db.update(
-            {"last_read_chapter": manga_info["last_chapter"]},
-            Manga.title.matches(manga_regex, re.IGNORECASE),
-        )
-
-
-def check_manga_in_list(manga_title):
+def check_manga_existence(manga_title):
     manga_regex = manga_title_regex_matcher(manga_title)
     Manga = Query()
 
@@ -69,15 +54,15 @@ def check_manga_in_list(manga_title):
 
 
 def manga_title_regex_matcher(manga_title):
-    regex = r""
+    regex = r"^"
 
     for manga in manga_title.split():
         regex += rf"{re.escape(manga)}\s"
 
-    regex = regex.rstrip("\\s")
+    regex = regex.rstrip("\\s") + "$"
 
     return regex
 
 
 if __name__ == "__main__":
-    print(check_manga_in_list("I became an evolving space monster"))
+    print(check_manga_existence("tower of god"))
